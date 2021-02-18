8ff72317799209038c2aa36c6f239b33fca0d132
Petar <petar.iliev.pii@gmail.com>
2021-02-18 15:30:20 +0100

    s

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 describe('Simple String Test', function () {
     it('should return number of charachters in a string', function () {
-           assert.equal("Hello World!".length, 122);
+           assert.equal("Hello World!".length, 12);
        });
-       it('should return number of charachters in a string', function () {
+       it('should return number of charachters in a string2', function () {
         assert.equal("Hello World!".length, 12);
     });
    });
